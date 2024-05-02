b50a4e38e71124ee7885570ea7b01c79ddd0ccb0
albertjasa <alberjasa@gmail.com>
2024-05-01 21:20:05 -0700

    More cors update

--- server/__init__.py
+++ server/__init__.py
@@ -51,14 +51,14 @@ import server.routes.sms
 
 
 
-# # Add Access-Control-Allow-Origin header to responses
-# @app.after_request
-# def after_request(response):
-#     response.headers.add("Access-Control-Allow-Origin", "*")
-#     response.headers.add("Access-Control-Allow-Credentials", "true")
-#     response.headers.add("Access-Control-Allow-Headers", "*")
-#     response.headers.add("Access-Control-Allow-Methods", "*")
-#     return response
+# Add Access-Control-Allow-Origin header to responses
+@app.after_request
+def after_request(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Credentials", "true")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
+    return response
 
 # import database models
 from server.models.user import User
