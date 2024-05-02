af0a54a2ddcee50bcd88085b0ac45e73534b5d8a
albertjasa <alberjasa@gmail.com>
2024-05-01 21:31:48 -0700

    Cors again

--- src/Api/Api.tsx
+++ src/Api/Api.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const api = axios.create({
   withCredentials: false,
-  baseURL: process.env.REACT_APP_IS_PRODUCTION === "yes" ? "http://chatterai.pythonanywhere.com/api/" : "http://127.0.0.1:5000/api",
+  baseURL: process.env.REACT_APP_IS_PRODUCTION === "yes" ? "http://chatterai.pythonanywhere.com/api" : "http://127.0.0.1:5000/api",
   // baseURL:"http://127.0.0.1:5000/api",
   headers: {
     'Access-Control-Allow-Origin': "*",
